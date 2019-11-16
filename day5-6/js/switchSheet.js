54f4699960c56346cb9444c3c3bb9fa93a6fccbb
chenchexm <chenche741@gmail.com>
2019-11-16 19:41:26 +0800

    day 5-6

--- day5-6/js/switchSheet.js
+++ day5-6/js/switchSheet.js
@@ -2,27 +2,30 @@ let btnList = document.getElementById("btnList");
 
 btnList.addEventListener("click", function(event) {
 	let target = event.target;
-	let fileName = null;
-	switch (target.id) {
-		case "sheet-1":
-			fileName = "css/style1.css";
-			break;
-		case "sheet-2":
-			fileName = "css/style2.css";
-			break;
-		case "sheet-3":
-			fileName = "css/style3.css";
-			break;
-		default:
-			fileName = null;
-	}
-	let linkNodes = document.getElementsByTagName("link");
-	for (let i in linkNodes) {
-		if (
-			linkNodes[i].getAttribute("rel") === "stylesheet" &&
-			linkNodes[i].getAttribute("href").indexOf("style") > -1
-		) {
-			linkNodes[i].setAttribute("href", fileName);
+	if (target.nodeName.toLocaleLowerCase() === "button") {
+		let fileName = "";
+		switch (target.id) {
+			case "sheet-1":
+				fileName = "css/style1.css";
+				break;
+			case "sheet-2":
+				fileName = "css/style2.css";
+				break;
+			case "sheet-3":
+				fileName = "css/style3.css";
+				break;
+			default:
+				fileName = "";
+		}
+		let linkNodes = document.getElementsByTagName("link");
+		for(let node of linkNodes){
+			console.log(node);
+			if (
+				node.getAttribute("rel") === "stylesheet" &&
+				node.getAttribute("href").indexOf("style") > -1
+			) {
+				node.setAttribute("href", fileName);
+			}
 		}
 	}
 });
